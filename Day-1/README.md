e14ba6657f55c6518a923ae14ebf28995fe4dfb3
prem <praulayar994@gmail.com>
2023-11-29 16:27:01 +0530

    connect to postgresql database

---
+++ Day-1/README.md
@@ -0,0 +1,7 @@
+# Connect to PostgreSQL from Python
+
+- Connect to PostgreSQL Database from Python using psycopg2 module and then perform some database transactions.
+- Create table, insert data to the table, update a table data, fecth data or remove data records from a python program.
+
+
+[Youtube Link](https://www.youtube.com/watch?v=M2NzvnfS-hI "Youtube")
---
+++ Day-1/db_connect.py
@@ -0,0 +1,65 @@
+import psycopg2
+import psycopg2.extras
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(
+        host=os.getenv('host'),
+        database=os.getenv('database'),
+        user=os.getenv('user'),
+        password=os.getenv('password'),
+        port=os.getenv('port')
+    )
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute('DROP TABLE IF EXISTS employee')
+
+    create_table = ''' CREATE TABLE IF NOT EXISTS employee (
+                            id int PRIMARY KEY,
+                            name varchar(40) NOT NULL,
+                            salary int,
+                            dept_id varchar(255) )'''
+
+    cur.execute(create_table)
+
+    insert_script = 'INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
+
+    insert_values = [(1, 'Prem', 50000, 'AI'), (2, 'Radhe', 60000, 'ML'), (3, 'Kabir', 70000, 'DL')]
+
+    for record in insert_values:
+        cur.execute(insert_script, record)
+
+    update_script = '''UPDATE employee 
+                       SET salary = salary+(salary*0.05)
+                       WHERE salary <= 55000''' 
+    
+    cur.execute(update_script)
+
+    delete_script = 'DELETE FROM employee WHERE name= %s'
+
+    cur.execute(delete_script, ('Kabir', ))
+
+    cur.execute('SELECT * FROM employee')
+    records = cur.fetchall()
+    for record in records:
+        print(record)
+        print(record['name'], record['salary'])
+
+    
+
+    conn.commit()
+
+except Exception as error:
+    print(error)
+
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
